bd5c12b63c460e11d2848083440feb2dd920dd5d
Joseph Herbert Tandingan <jh.tandingan@coi.local>
2019-03-07 15:32:10 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  address: string = '';
+
+  constructor(private router: Router) { }
+
+  goToDetail() {
+    this.router.navigate(['/detail']);
+  }
+
+  // handleClick() {
+  //   alert(this.address);
+  // }
 
 }
